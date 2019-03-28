a9f46d5f351dc4d12edef29f8a3563b6c33abcbe
Jerry <wzjing103@163.com>
2019-03-28 17:59:04 +0800

    update layout

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
 import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/base16-light.css';
+import 'codemirror/theme/idea.css';
 import '../stylesheets/index.scss';
 import '../stylesheets/editor.scss'
 
@@ -8,48 +8,85 @@ import 'codemirror/mode/javascript/javascript.js'
 import CodeMirror from 'codemirror/lib/codemirror.js';
 import {getProperty} from "./lib/animation";
 
-document.querySelector('.icon-play').innerHTML = iconRun;
+let editor;
 
-let editor = new CodeMirror(document.querySelector('#editor'), {
-    mode: 'javascript',
-    theme: 'infinity',
-    value: `
-/* value from [0, 1] */
+function initEditor() {
+    document.querySelector('.icon-play').innerHTML = iconRun;
+    editor = new CodeMirror(document.querySelector('#editor'), {
+        mode: 'javascript',
+        theme: 'infinity',
+        value: `/* value from [0, 1] */
 
 function println(num, subfix) {
   console.log(\`\${num} \${subfix}\`);
 }
 
 function interpolator(value) {
-  let v = Math.sin(value * Math.PI/2);
+  let v = Math.sin(value * Math.PI);
   console.log(\`current: \${v}\`);
-  for(let i=0; i<10; i++) {
-    println(i, 'times');
-  }
   return v;
+}`
+    });
 }
-`
-});
 
-let canvas = document.querySelector('#graph');
-let w = getProperty(canvas, 'width').get();
-let h = getProperty(canvas, 'height').get();
-console.log(`Size: [${w} x ${h}]`);
-let ctx = canvas.getContext('2d');
-ctx.canvas.width = w;
-ctx.canvas.height = h;
-let x = (p) => {return w*p};
-let y = (p) => {return h*p};
+function drawGraph() {
+    let canvas = document.querySelector('#graph');
+    let w = getProperty(canvas, 'width').get();
+    let h = getProperty(canvas, 'height').get();
+    console.log(`Size: [${w} x ${h}]`);
+    let ctx = canvas.getContext('2d');
+    canvas.width = w;
+    canvas.height = h;
 
-ctx.strokeStyle = '#000000';
-ctx.lineWidth = 0.5;
-ctx.beginPath();
-let margin = y(0.1);
-ctx.moveTo(margin, margin);
-ctx.lineTo(margin, h-margin);
-ctx.lineTo(w-margin, h-margin);
-ctx.stroke();
-ctx.strokeStyle = '#003bf3';
-ctx.textAlign = 'center';
-ctx.font = 'normal 12px';
-ctx.strokeText('v', margin/2, margin, margin/2);
+    ctx.strokeStyle = '#000000';
+    ctx.lineWidth = 0.5;
+    ctx.beginPath();
+    let margin = h * 0.15;
+    ctx.moveTo(margin, margin);
+    ctx.lineTo(margin, h - margin);
+    ctx.lineTo(w - margin, h - margin);
+    ctx.stroke();
+    ctx.fillStyle = '#000000';
+    ctx.textAlign = 'center';
+    ctx.font = '14px Arial';
+    ctx.textBaseline = 'middle';
+    ctx.fillText('s', margin / 2, margin);
+    ctx.fillText('t', w - margin, h - margin / 2, 48);
+
+    let x = (p) => {
+        return (w - 2 * margin) * p + margin
+    };
+    let y = (p) => {
+        return h - (h - 2 * margin) * p - margin
+    };
+    // draw graph
+    ctx.strokeStyle = '#2979FF';
+    ctx.moveTo(x(0), y(0));
+    ctx.beginPath();
+    for (let i = 0; i < 1; i += 0.1) {
+        // ctx.arc(x(i), y(interpolator(i)), 3, 0, 360, false);
+        ctx.lineTo(x(i), y(interpolator(i)));
+    }
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function run() {
+    if (editor !== null) {
+        let code = editor.getValue();
+        if (/function interpolator\([_a-z0-9]+\)/.test(code)) {
+            document.querySelector('#interpolator').innerHTML = code;
+            drawGraph();
+        } else {
+            window.alert('Wrong format, no interpolator function found');
+        }
+    }
+}
+
+window.onload = () => {
+    initEditor();
+    document.querySelector('.code-editor-run').addEventListener('click', () => {
+        run();
+    });
+};
+
